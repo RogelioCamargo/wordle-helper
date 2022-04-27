4bfd7a637132383a51906e6f4b3e193af9645f4e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-04-27 10:34:17 -0700

    Refactor Grid component

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,18 +1,23 @@
 import Cell from "./Cell";
+import EmptyCell from "./EmptyCell";
 
-const Grid = ({ values, colors, changeColor }) => {
+const Grid = ({ values, changeColor }) => {
+	const MAX_CELLS = 30;
+	const VALUE_LENGTH = values.length;
 	const cells = [];
-	for (let i = 0; i < 30; i++) {
+
+	// create value cells 
+	for (let i = 0; i < VALUE_LENGTH; i++) {
 		cells.push(
-			<Cell
-				changeColor={changeColor}
-				colorValue={colors[i]}
-				index={i}
-				key={i}
-				value={values[i]}
-			/>
+			<Cell changeColor={changeColor} index={i} key={i} character={values[i]} />
 		);
 	}
+
+	// create empty cells  
+	for (let i = VALUE_LENGTH; i < MAX_CELLS; i++) {
+		cells.push(<EmptyCell key={i} />);
+	}
+
 	return (
 		<div className="flex justify-center items-center flex-grow">
 			<div className="h-board w-board grid grid-cols-5 gap-1">{cells}</div>
